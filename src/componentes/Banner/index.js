0472d4db280b838630b1e2b5fd8034a55dce625e
Gabriela Albuquerque <126294239+gabriela-albuquerque@users.noreply.github.com>
2024-11-16 11:52:20 -0300

    implementacao do componente banner

--- src/componentes/Banner/index.js
+++ src/componentes/Banner/index.js
@@ -1,16 +1,17 @@
 import styles from "./Banner.module.css";
 import circuloColorido from "assets/circulo_colorido.png";
+import minhaFoto from "assets/minha_foto.png";
 
 export default function Banner() {
   return (
-    <div className={styles.Banner}>
+    <div className={styles.banner}>
       <div className={styles.apresentacao}>
         <h1 className={styles.titulo}>Olá Mundo!</h1>
 
         <p className={styles.paragrafo}>
-          Boas vindas ao meu espaço pessoal! Eu sou Antônio Evaldo, instrutor de
+          {`Boas vindas ao meu espaço pessoal! Eu sou Antônio Evaldo, instrutor de
           Front-end da Alura. Aqui compartilho vários conhecimentos, espero que
-          aprenda algo novo :)
+          aprenda algo novo :)`}
         </p>
       </div>
 
@@ -18,7 +19,13 @@ export default function Banner() {
         <img
           className={styles.circuloColorido}
           src={circuloColorido}
+          alt=""
           aria-hidden={true}
+        />
+        <img
+          className={styles.minhaFoto}
+          src={minhaFoto}
+          alt="Foto do Antonio Evaldo sorrindo"
         />
       </div>
     </div>
